fab036707e8917e433d594fa128842a5fce3d572
Ethan Jones <ejones@cse.unl.edu>
2019-09-13 16:35:31 -0500

    Added helloName.c

---
+++ helloName.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+Note: I didn't fully look at this part until Friday, so I didn't realize this
+required a partner until after I had lost the ability to communicate with
+anyone from the class. This part will only be me.
+*/
+int main(int argc, char **argv) {
+
+  printf("Ethan Jones");
+
+}
